05f135e10a3a341134d91c858fa02f0876863d7e
Amir hossein khavari <159920615+Amirkhavari25@users.noreply.github.com>
2025-01-22 13:14:53 +0330

    first step of implementing an end point for graphql

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql'); // اصلاح این خط
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/graphAPI', graphqlHTTP({
+    schema: buildSchema(`
+        type QueryRoot{
+            users : [String!]!
+        }
+
+        type MutationRoot{
+            createUser(name:String) : String
+        }
+
+        schema {
+            query:QueryRoot
+            mutation:MutationRoot
+        }
+    `),
+    rootValue: {
+        users: () => {
+            return ['user1', 'user2'];
+        },
+        createUser: (args) => {
+            return args.name;
+        }
+    },
+    graphiql: true,
+}))
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
